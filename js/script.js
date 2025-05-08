fb8b53f14fc9e5bb03b47712627ff0bfcce3b9fd
Luis Morgado <siuldesing@gmail.com>
2025-05-08 19:56:42 -0400

    some visual improvements were added and the start of new lines of javaScript was half-done,

--- js/script.js
+++ js/script.js
@@ -1,1 +1,27 @@
+const CONTAINER = document.querySelector("#container")
+const MESSAGES = document.querySelector("#messages")
+const BUTTON = document.querySelector("#button")
+const FORM = document.querySelector("#form")
+const INPUT = document.querySelector("#textUserArea")
+const TEMPLATE = document.querySelector("#template")
 
+FORM.addEventListener("submit", Event => {
+  Event.preventDefault()
+  const msgText = INPUT.value.trim()
+
+  if (msgText !== "") {
+    INPUT.value = ""
+  }
+})
+
+function addMessage(text, sender) {
+  const clonedTemplate = TEMPLATE.content.cloneNode(true)
+  const newMessage = clonedTemplate.querySelector(".template__msg")
+
+  const who = newMessage.querySelector("span")
+  const textParagraphs = newMessage.querySelector("p")
+
+  textParagraphs.textContent = text
+  who.textContent = sender
+  newMessage.classList.add(sender)
+}
